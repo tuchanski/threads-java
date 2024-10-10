72e0a34ec0b9008eef3bff4148c7ae44bb31332f
Tuchanski <gui.rocha2004@gmail.com>
2024-10-10 11:51:49 -0300

    Fix Implementation2

--- Implementation2/src/application/Main.java
+++ Implementation2/src/application/Main.java
@@ -5,12 +5,12 @@ import model.MyThread;
 public class Main {
     public static void main(String[] args) {
 
-        MyThread t1 = new MyThread("Guilherme");
-        MyThread t2 = new MyThread("Joaquim");
+        Thread t1 = new Thread(new MyThread("Guilherme"));
+        Thread t2 = new Thread(new MyThread("Joaquim"));
 
         for (int i = 0; i < 3; i++) {
-            t1.run();
-            t2.run();
+            t1.start();
+            t2.start();
         }
 
     }
